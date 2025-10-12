faf108c298e045285cb1aedec7dab15a8d22e12b
Shahed Ahamed <shahedahamed.official@gmail.com>
2025-10-12 16:33:04 +0600

    initial commit

---
+++ round-robin.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum)
+{
+    
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
+        rem_bt[i] = bt[i];
+
+    int t = 0; 
+    while (1)
+    {
+        bool done = true;
+
+       
+        for (int i = 0; i < n; i++)
+        {
+            
+            if (rem_bt[i] > 0)
+            {
+                done = false;
+
+                if (rem_bt[i] > quantum)
+                {
+                   
+                    t += quantum;
+
+                    
+                    rem_bt[i] -= quantum;
+                }
+
+                
+                else
+                {
+                  
+                    t = t + rem_bt[i];
+
+                    wt[i] = t - bt[i];
+
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+
+     
